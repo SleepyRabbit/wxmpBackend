e8033c615ef5568c9900e94ce34429d8e43f564a
侯恩星 <houenxing@houenxingdeMacBook-Pro.local>
2018-06-22 16:35:51 +0800

    commit

---
+++ routes/login.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request')
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+
+    var appid = "wxd9dd7a39008b6d48";
+    var appsecret = "305fb1d59364309f40713d74cc6a7e0c";
+
+    var url ='https://api.weixin.qq.com/sns/jscode2session'
+        + '?appid=' + appid +
+        '&secret=' + appsecret +
+        '&js_code=' + req.body.code +
+        '&grant_type=authorization_code';
+
+    var options = {
+        url: url,
+        method: 'POST',
+    };
+
+    request(url, (err, respond, body) => {
+        var data = {
+            err: err,
+            data: {},
+        }
+        // console.log(err);
+        if(!err && respond.statusCode == 200) {
+            // console.log(body);
+            data.data = body;
+        }
+        res.send(data);
+    })
+});
+
+module.exports = router;
